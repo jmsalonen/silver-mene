daebacc7af30b1c977358b630d4f33e181f5c5b9
Janne Salonen <janne.salonen@avanio.com>
2024-10-11 02:03:07 +0300

    init backend

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "../client/build")));
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use("*", (req, res) => {
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
